3fa78ad077a51084e32f93c5bb7b7bce89bc0a7f
Giyoon Park <workspaceforict@gmail.com>
2021-11-06 23:44:51 +0900

    Update: initial web page

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,21 @@
+import { Button } from '@material-ui/core';
 import React from 'react'
 import './Login.css';
+import {signIn} from "./firebase";
+
 
 function Login() {
     return (
         <div className="login">
-            
+            <div className="login__container">
+                <img 
+                    src="img/Slack_Mark_Web.png"
+                    alt=""
+                />
+            <h1>Sign in to Fake Slack</h1>
+            <p>yoon.slack.com</p>
+            <Button onClick={signIn}>Sign in with Google</Button>
+            </div>
         </div>
     )
 }
